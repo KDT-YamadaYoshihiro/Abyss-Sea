eead38608ce0ce95f1912e048aea6e20964047ac
yamada <KDT24GPR30005@edu.kobe-tech.ac.jp>
2025-10-11 03:04:06 +0900

    カットインクラスの生成準備

---
+++ src/CutIn.cpp
@@ -0,0 +1,13 @@
+#include "CutIn.h"
+
+void Cutuin::Start()
+{
+}
+
+void Cutuin::Update()
+{
+}
+
+void Cutuin::Draw()
+{
+}
--- src/CutIn.h
+++ src/CutIn.h
@@ -2,6 +2,41 @@
 
 class Cutuin{
 
+	// 表示フラグ
+	bool drawFlag;
+
+	// 座標
+	int x, y;
+
+	// サイズ
+	int sizeW, sizeH; 
+
+	// 画像ハンドル
+	int handle;
+
+	// アニメーションフレーム
+	int frame;
+
+public:
+
+	// 初期化
+	Cutuin(int arg_x, int arg_y, int arg_sizeW, int arg_sizeH, int arg_handle)
+		:drawFlag(false), x(arg_x), y(arg_y), sizeW(arg_sizeW), sizeH(arg_sizeH), handle(arg_handle), frame(0)
+	{
+
+	};
+
+	// デストラクタ
+	virtual ~Cutuin() {};
+
+	// スタート関数
+	void Start();
+
+	// 更新
+	void Update();
+
+	// 描画
+	void Draw();
 
 
 };
